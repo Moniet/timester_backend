5bf8a81d921531aaa80a74b5c041587d20f7b5eb
moniet <moniet.sawhney@gmail.com>
2019-08-22 16:33:17 +0100

    added ENV var to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     before_action :authorized
 
     def encode_token(payload)
-        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        JWT.encode(payload, ENV['SECRET_KEY_BASE'])
     end
 
     def auth_header
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(' ')[1]
             begin
-                JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+                JWT.decode(token, ENV['SECRET_KEY_BASE'], true, algorithm: 'HS256')
             rescue JWT::DecodeError
                 nil
             end
